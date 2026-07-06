ff5f2826c7f86b321ddb6739f394b82be27fa5a1
Ammar Mustaqeem <ammarmustaqeem38@gmail.com>
2026-07-07 03:45:41 +0500

    scrappy character anim added, machi tower travel pre-made

--- Player.cs
+++ Player.cs
@@ -1,50 +1,16 @@
-//using Godot;
-//using System;
-//
-//public partial class Player : CharacterBody2D
-//{
-	//public const float Speed = 300.0f;
-	//public const float JumpVelocity = -400.0f;
-//
-	//public override void _PhysicsProcess(double delta)
-	//{
-		//Vector2 velocity = Velocity;
-//
-		//// Add the gravity.
-		//if (!IsOnFloor())
-		//{
-			//velocity += GetGravity() * (float)delta;
-		//}
-//
-		//// Handle Jump.
-		//if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		//{
-			//velocity.Y = JumpVelocity;
-		//}
-//
-		//// Get the input direction and handle the movement/deceleration.
-		//// As good practice, you should replace UI actions with custom gameplay actions.
-		//Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		//if (direction != Vector2.Zero)
-		//{
-			//velocity.X = direction.X * Speed;
-		//}
-		//else
-		//{
-			//velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		//}
-//
-		//Velocity = velocity;
-		//MoveAndSlide();
-	//}
-//}
-
 using Godot;
 
 public partial class Player : CharacterBody2D
 {
-	[Export]
+	[Export] 
 	public float Speed = 400f;
+
+	private AnimatedSprite2D anim;
+
+	public override void _Ready()
+	{
+		anim = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -52,17 +18,54 @@ public partial class Player : CharacterBody2D
 
 		if (Input.IsActionPressed("ui_right"))
 			direction.X += 1;
-
 		if (Input.IsActionPressed("ui_left"))
 			direction.X -= 1;
-
 		if (Input.IsActionPressed("ui_down"))
 			direction.Y += 1;
-
 		if (Input.IsActionPressed("ui_up"))
 			direction.Y -= 1;
 
-		Velocity = direction.Normalized() * Speed;
+		if (direction != Vector2.Zero)
+		{
+			Velocity = direction.Normalized() * Speed;
+			UpdateAnimation(direction);
+		}
+		else
+		{
+			Velocity = Vector2.Zero;
+			anim.Stop(); 
+		}
+
 		MoveAndSlide();
+	}
+
+	private void UpdateAnimation(Vector2 direction)
+	{
+		// Reset horizontal flipping
+		anim.FlipH = false;
+
+		// Prioritize horizontal movement animations
+		if (direction.X > 0)
+		{
+			anim.Play("walk_right");
+		}
+		else if (direction.X < 0)
+		{
+			// If you have a separate "walk_left" animation:
+			anim.Play("walk_left");
+			
+			// OR if you want to reuse "walk_right" flipped, uncomment below:
+			// anim.Play("walk_right");
+			// anim.FlipH = true;
+		}
+		// Handle vertical movement animations
+		else if (direction.Y > 0)
+		{
+			anim.Play("walk_down");
+		}
+		else if (direction.Y < 0)
+		{
+			anim.Play("walk_up");
+		}
 	}
 }
