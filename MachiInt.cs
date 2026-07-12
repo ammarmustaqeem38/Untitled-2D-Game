db8f72c4a3efcbc98dde2fc05b6097074d38225f
Ammar Mustaqeem <ammarmustaqeem38@gmail.com>
2026-07-12 19:16:41 +0500

    add machiInt preliminary details

--- MachiInt.cs
+++ MachiInt.cs
@@ -2,6 +2,9 @@ using Godot;
 
 public partial class MachiInt : Node2D
 {
+	private Polygon2D tvLightStrobe;
+	private double strobeTime;
+
 	public override void _Ready()
 	{
 		var player = GetNode<CharacterBody2D>("/root/Player");
@@ -9,5 +12,18 @@ public partial class MachiInt : Node2D
 		//var audioPlayer = GetNode<AudioStreamPlayer2D>("AudioStreamPlayer2D");
 		//audioPlayer.Play();
 		player.GlobalPosition = spawn.GlobalPosition;
+		tvLightStrobe = GetNodeOrNull<Polygon2D>("Lighting/TVLightStrobe");
+	}
+
+	public override void _Process(double delta)
+	{
+		if (tvLightStrobe == null)
+		{
+			return;
+		}
+
+		strobeTime += delta;
+		var alpha = 0.16f + 0.12f * (1.0f + Mathf.Sin((float)strobeTime * 9.0f)) * 0.5f;
+		tvLightStrobe.Color = new Color(0.26f, 0.62f, 1.0f, alpha);
 	}
 }
